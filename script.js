e18b67f364f52f2998c463383993640e69206b3a
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-08-28 15:37:45 -0300

    site responsivo

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.menu');
+
+function apareceMenu(){
+    if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }
+    else{
+        menu.style.display = 'block'
+    }
+
+}
